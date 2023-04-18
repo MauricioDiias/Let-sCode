63854411fa0035a4a6811d5f2aad4f1256da3a20
MauricioDiias <mauriciod664@gmail.com>
2023-04-17 22:24:36 -0300

    atualizar aulas

---
+++ js/Aula 05/index.js
@@ -0,0 +1,30 @@
+
+
+let n1 = 7;
+let n2 = 8;
+
+let maior = n1 > n2 ? n1 : n2;
+
+console.log('Maior valor é:',maior)
+
+
+function multiplicacao(numero1,numero2){
+    return numero1 * numero2
+}
+
+function divisao(numero1,numero2){
+    return numero1 / numero2
+}
+
+function subtracao(numero1,numero2){
+    return numero1 - numero2
+}
+
+function soma(numero1,numero2){
+    return numero1 + numero2
+}
+
+console.log(soma(5,3))
+console.log(subtracao(5,3))
+console.log(multiplicacao(5,3))
+console.log(divisao(5,3))
